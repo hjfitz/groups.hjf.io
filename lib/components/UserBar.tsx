f76172ca42dded3ae279e27dd3a97e847660b668
Harry Fitzgerald <harry.fitzgerald@truepill.com>
2021-12-13 10:17:57 +0000

    builds be done

--- lib/components/UserBar.tsx
+++ lib/components/UserBar.tsx
@@ -1,62 +1,66 @@
-import React from 'react'
-import {useLocation} from '@reach/router'
-import ChatBox from '@/components/chat'
-import {copy} from '@/util'
-import {selectShareLink} from '@/state/slices/metadata'
-import {useAppSelector} from '@/state/hooks'
+import React from "react";
+import { useLocation } from "@reach/router";
+//import ChatBox from '@/components/chat'
+import { copy } from "@/util";
+import { selectShareLink } from "@/state/slices/metadata";
+import { useAppSelector } from "@/state/hooks";
 
-const {useState} = React
+const { useState } = React;
 
 interface BarProps {
-	stream?: MediaStream
+  stream?: MediaStream;
 }
 
 const UserBar = (props: BarProps) => {
-	// hack but no other way to give multiple paths as a prop
-	const location = useLocation()
-	const allowedRoutes = ['/host', '/room']
-	if (!allowedRoutes.some((route) => location.pathname.includes(route))) return <></>
+  // hack but no other way to give multiple paths as a prop
+  const location = useLocation();
+  const allowedRoutes = ["/host", "/room"];
+  if (!allowedRoutes.some((route) => location.pathname.includes(route)))
+    return <></>;
 
-	const url = useAppSelector(selectShareLink)
+  const url = useAppSelector(selectShareLink);
 
-	const [showChat, setShowChat] = useState<boolean>(true)
-	const toggleChat = () => setShowChat((cur) => !cur)
+  //const [showChat, setShowChat] = useState<boolean>(true)
+  //const toggleChat = () => setShowChat((cur) => !cur)
 
-	const [muted, setMute] = useState<boolean>(false)
-	function toggleMute() {
-		const {stream} = props
-		if (!stream) return
-		setMute((current) => {
-			/* eslint-disable no-param-reassign */
-			stream.getAudioTracks().forEach((track) => track.enabled = current)
-			return !current
-		})
-	}
+  const [muted, setMute] = useState<boolean>(false);
+  function toggleMute() {
+    const { stream } = props;
+    if (!stream) return;
+    setMute((current) => {
+      /* eslint-disable no-param-reassign */
+      stream.getAudioTracks().forEach((track) => (track.enabled = current));
+      return !current;
+    });
+  }
 
-	let muteContent = <span className="mx-auto text-green-300">Speaking</span>
-	if (muted) muteContent = <span className="mx-auto text-red-300">Muted</span>
+  let muteContent = <span className="mx-auto text-green-300">Speaking</span>;
+  if (muted) muteContent = <span className="mx-auto text-red-300">Muted</span>;
 
-	return (
-		<div className="userbar">
-			<section className="w-full h-full bg-black ">
-				<div className="flex items-center justify-between h-full px-4">
-					<div
-						onClick={toggleMute}
-						className="flex items-center w-24 h-full text-center cursor-pointer hover:bg-gray-800 transition duration-300"
-					>
-						{muteContent}
-					</div>
-					{/* <div>
+  return (
+    <div className="userbar">
+      <section className="w-full h-full bg-black ">
+        <div className="flex items-center justify-between h-full px-4">
+          <div
+            onClick={toggleMute}
+            className="flex items-center w-24 h-full text-center cursor-pointer hover:bg-gray-800 transition duration-300"
+          >
+            {muteContent}
+          </div>
+          {/* <div>
 						<ChatBox show={showChat} />
 						<span onClick={toggleChat} className="cursor-pointer">Show Chat</span>
 					</div> */}
-					<div>
-						<span>Link to room: </span><span className="cursor-pointer" onClick={copy(url)}>{url}</span>
-					</div>
-				</div>
-			</section>
-		</div>
-	)
-}
+          <div>
+            <span>Link to room: </span>
+            <span className="cursor-pointer" onClick={copy(url)}>
+              {url}
+            </span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default UserBar
+export default UserBar;
--- lib/components/chat.tsx
+++ lib/components/chat.tsx
@@ -1,77 +1,75 @@
-import {HostPayload, MessagePayload} from '@/routes/types'
-import {useAppSelector} from '@/state/hooks'
-import {selectHost} from '@/state/slices/metadata'
-import {DataConnection} from 'peerjs'
-import React, {useEffect, useState} from 'react'
-import {peer} from '@/state/globals'
+import { HostPayload, MessagePayload } from "@/routes/types";
+import { useAppSelector } from "@/state/hooks";
+import { selectHost } from "@/state/slices/metadata";
+import { DataConnection } from "peerjs";
+import React, { useEffect, useState } from "react";
+import { peer } from "@/state/globals";
 
 interface ChatProps {
-	show: boolean
+  show: boolean;
 }
 
 interface MessageIn {
-	who: string
-	message: string
+  who: string;
+  message: string;
 }
 
 const ChatIn: React.FC = () => {
-	const host = useAppSelector(selectHost)
-	function handleInput(ev) {
-		if (ev.key !== 'Enter') return
-		console.log(ev.target.value)
-		// emit to host
-	}
-	return (
-		<input
-			className="px-2 bg-gray-800 rounded"
-			onKeyUp={handleInput}
-		/>
-	)
-}
+  //const host = useAppSelector(selectHost)
+  // @ts-expect-error lol types
+  function handleInput(ev) {
+    if (ev.key !== "Enter") return;
+    console.log(ev.target.value);
+    // emit to host
+  }
+  return <input className="px-2 bg-gray-800 rounded" onKeyUp={handleInput} />;
+};
 
-const Message = ({who, message}: MessageIn) => (
-	<div>
-		<span>{who}: </span>
-		<span>{message}</span>
-	</div>
-)
+const Message = ({ who, message }: MessageIn) => (
+  <div>
+    <span>{who}: </span>
+    <span>{message}</span>
+  </div>
+);
 
 // temp dev
 const defaultState: MessageIn[] = [
-	{who: 'harry', message: 'hey'},
-	{who: 'george', message: 'hey'},
-	{who: 'harry', message: 'how are you'},
-	{who: 'george', message: 'yea good'},
-	{who: 'george', message: 'you??'},
-]
+  { who: "harry", message: "hey" },
+  { who: "george", message: "hey" },
+  { who: "harry", message: "how are you" },
+  { who: "george", message: "yea good" },
+  { who: "george", message: "you??" },
+];
 
-const ChatBox: React.FC<ChatProps> = ({show}: ChatProps) => {
-	const [messages, setMessages] = useState<MessageIn[]>(defaultState)
-	useEffect(() => {
-		if (!peer) return
-		// do stuff
-		peer.on('connection', (conn: DataConnection) => {
-			conn.on('data', (data) => {
-				const payload = JSON.parse(data) as HostPayload
-				if (payload.event !== 'message.send') return
-				const messagePayload = payload as MessagePayload
-				setMessages((mgs) => [
-					...mgs,
-					{who: messagePayload.who, message: messagePayload.message},
-				])
-			})
-		})
-	}, [peer])
-	let displayClass = 'block'
-	if (!show) displayClass = 'hidden'
-	return (
-		<section className={`relative ${displayClass}`}>
-			<div className="absolute bottom-0 flex flex-col justify-end h-64 p-4">
-				{messages.map((message) => <Message {...message} />)}
-				<ChatIn />
-			</div>
-		</section>
-	)
-}
+const ChatBox: React.FC<ChatProps> = ({ show }: ChatProps) => {
+  const [messages, setMessages] = useState<MessageIn[]>(defaultState);
+  useEffect(() => {
+    if (!peer) return;
+    // do stuff
+    peer.on("connection", (conn: DataConnection) => {
+      conn.on("data", (data) => {
+        const payload = JSON.parse(data) as HostPayload;
+        if (payload.event !== "message.send") return;
+        const messagePayload = payload as MessagePayload;
+        setMessages((mgs) => [
+          ...mgs,
+          { who: messagePayload.who, message: messagePayload.message },
+        ]);
+      });
+    });
+  }, [peer]);
+  let displayClass = "block";
+  if (!show) displayClass = "hidden";
+  return (
+    <section className={`relative ${displayClass}`}>
+      <div className="absolute bottom-0 flex flex-col justify-end h-64 p-4">
+        {messages.map((message) => (
+          <Message {...message} />
+        ))}
+        <ChatIn />
+      </div>
+    </section>
+  );
+};
 
-export default ChatBox
+export default ChatBox;
